8d8948237c1aacc78dd989b8581cdfc311b935ab
TerriLynn Chaffey <terrilynnchaffey@gmail.com>
2023-03-21 11:27:17 -0230

    Terri-Lynn Chaffey

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import ResProvider from "./components/ResContext";
 
 ReactDOM.render(
   <React.StrictMode>
